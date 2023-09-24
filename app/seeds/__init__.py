f6d2116491401dfc4eb782698b89705f52287a7e
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-09-24 13:25:55 -0400

    Isolate1

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,8 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .listing import seed_listings, undo_listings  # Import listings seed functions
-from .showing import seed_showings, undo_showings  # Import showing seed functions
-
+from .showings import seed_showings, undo_showings
 from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
@@ -17,16 +16,16 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_listings()
+        undo_showings()
+        undo_listings() 
         undo_users()
-        undo_showings() 
     seed_users()
-    seed_listings()  
-    seed_showings() 
+    seed_listings()
+    seed_showings()
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
-    undo_listings()  # Undo listings data
-    undo_showings()  # Add this line to undo showing data
+    undo_listings()  
+    undo_showings()
