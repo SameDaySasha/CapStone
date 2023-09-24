04937f6d2566c5526aee7d03f90d685b74ecbed4
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-09-24 13:09:37 -0400

    Maybe?

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from .db import db
 from .user import User
 from .listing import Listing  # Adding this line to import the Listing model
+from .showing import Showing
 from .db import environment, SCHEMA
