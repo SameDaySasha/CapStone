1e9f0929e16bb04dbc052e89955ad9f7a1db337c
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-10-06 15:57:22 -0400

    Bug1Done

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -3,14 +3,12 @@ from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
-
 def user_exists(form, field):
     # Checking if user exists
     email = field.data
     user = User.query.filter(User.email == email).first()
     if user:
         raise ValidationError('Email address is already in use.')
-
 
 def username_exists(form, field):
     # Checking if username is already in use
@@ -19,9 +17,8 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
-
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField('email', validators=[DataRequired(), Email(message="Invalid email address"), user_exists])
     password = StringField('password', validators=[DataRequired()])
