1286f17c1d56fc50f0773d320592f504e027cbc5
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-10-07 18:15:46 -0400

    PleaseWork

--- react-app/src/components/homepage/banner.js
+++ react-app/src/components/homepage/banner.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux'; // Add this import
+import { useSelector } from 'react-redux';
 import ProfileButton from '../Navigation/ProfileButton';
 import './Banner.css';
 
 const Banner = ({isLoaded}) => {
   const history = useHistory();
-  const user = useSelector((state) => state.session.user); // Add this line to get current user
+  const user = useSelector((state) => state.session.user);
 
   const handleLogoClick = () => {
     history.push('/');
@@ -15,7 +15,7 @@ const Banner = ({isLoaded}) => {
   return (
     <div className="banner">
       <div className="banner-logo" onClick={handleLogoClick}>
-        My Logo
+        BlandWatch Land Auctions
       </div>
       <div>
         <ProfileButton user={user} /> 
